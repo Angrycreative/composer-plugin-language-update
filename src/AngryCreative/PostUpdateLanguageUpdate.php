372210c911727fd3d3558e7b8c4007b27f0d9c8e
Richard Sweeney <richard.sweeney@angrycreative.se>
2017-10-13 11:26:45 +0200

    Trying to get autoloader working...

---
+++ src/AngryCreative/PostUpdateLanguageUpdate.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: richardsweeney
+ * Date: 2017-10-13
+ * Time: 08:04
+ */
+
+namespace AngryCreative;
+
+use Composer\Installer\PackageEvent;
+
+class PostUpdateLanguageUpdate {
+
+	/**
+	 * @param PackageEvent $event
+	 */
+	public static function updateT10ns(PackageEvent $event) {
+		$installedPackage = $event->getOperation()->getPackage();
+
+		var_dump( $installedPackage );
+	}
+
+	/**
+	 * @param PackageEvent $event
+	 */
+	public static function removeT10ns(PackageEvent $event) {
+		$installedPackage = $event->getOperation()->getPackage();
+
+		var_dump( $installedPackage );
+	}
+
+}
